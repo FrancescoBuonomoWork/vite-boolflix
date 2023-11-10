b0c43acec216a7ce6c0ba52bce6ce2b40b9fbef5
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-11-10 16:27:04 +0100

    add flag

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -2,26 +2,61 @@
 
 export default {
     name: 'AppCard',
-    props:{
+    props: {
         film: Object
+    },
+    data() {
+        return {
+            languages: [
+                {
+                    lang: 'it',
+                    flagsSRC: "../../public/Flag_of_Italy.svg.png",
+                },
+                {
+                    lang: 'en',
+                    flagsSRC: "../../public/Flag_of_the_United_Kingdom.svg.png",
+                },
+                {
+                    lang: 'sp',
+                    flagsSRC: "../../public/Flag_of_Spain.svg.png"
+                },
+
+
+
+            ]
+        }
+    },
+    methods: {
+        // getFlag(){
+        //     v-if="film.original_language === 'it'"
+
+        // }
     }
 }
 </script>
 
 <template>
-    
-  <div class="card">
-    <p>{{ film.title }}</p>
-    <p>{{ film.original_title }}</p>
-    <p>{{ film.original_language }}</p>
-    <p>{{ film.vote_average }}</p>
+    <div class="card">
+        <p>{{ film.title }}</p>
+        <p>{{ film.original_title }}</p>
+        <p>{{ film.original_language }}
+            <img  v-for="language in languages" class="flag" :src="language.flagsSRC">
+            <!-- <img class="flag" src="../../public/Flag_of_the_United_Kingdom.svg.png">
+            <img class="flag" src="../../public/Flag_of_Spain.svg.png"> -->
 
-  </div>
-  
+        </p>
+        <p>{{ film.vote_average }}</p>
+
+    </div>
 </template>
 
 <style scoped lang="scss">
-.card{
+.card {
     padding: 10px 5px;
+
+    .flag {
+        width: 24px;
+        height: 16px;
+    }
 }
 </style>
