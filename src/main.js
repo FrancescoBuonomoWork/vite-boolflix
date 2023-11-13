e3c3f0361df11a8a2c33f4e86265f6c48f1f3672
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-11-13 13:00:49 +0100

    add regular stars

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
-// import { faStarR } from '@fortawesome/free-regular-svg-icons'
-library.add(faStar, )
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
+library.add(fasStar,farStar )
 
 
 createApp(App)
